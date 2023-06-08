29fcf158074237d5d22151f288b9e873dad31617
PlumpAndProud <98545393+PlumpAndProud@users.noreply.github.com>
2023-06-08 12:12:19 +0200

    1.1
    + nav
    + footer (now pos fixed)

--- Websites/scripts.js
+++ Websites/scripts.js
@@ -1,1 +1,13 @@
-//a tu pusto B)
+let scrollY = window.scrollY;
+
+document.addEventListener("scroll", ()=>{
+    let newScrollY = window.scrollY;
+
+    if(newScrollY < scrollY){
+        console.log("up");
+        scrollY = newScrollY;
+    } else {
+        console.log("down");
+        scrollY = newScrollY;
+    }
+})
