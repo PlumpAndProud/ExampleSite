17130fa0d9bb076804cbc6c3b1d5b1e64fc5b652
PlumpAndProud <f.machi03@gmail.com>
2023-06-26 13:58:34 +0200

    +jquery, size.js -> api.js , +imiona i nazwiska z api

---
+++ Site/content/js/api.js
@@ -0,0 +1,31 @@
+let blockTable = ['.first-block', '.second-block', '.third-block', '.fourth-block'];
+let textTable = ['first-text', 'second-text', 'third-text', 'fourth-text'];
+
+$.ajax({
+    url: 'https://randomuser.me/api/?results=4&inc=name',
+    dataType: 'json',
+    success: function (data) {
+
+        for (let i = 0; i < blockTable.length; i++) {
+            let $box = $(blockTable[i]);
+            $box.append(`<div class="text-1 ${textTable[i]}">` + data.results[i].name.first + ' ' + data.results[i].name.last + '</div>');
+        }
+
+        setWidth();
+    }
+});
+
+
+function setWidth() {
+
+    const wid = 174;
+
+    for (let i = 0; i < blockTable.length; i++) {
+
+        let curBlock = document.querySelector('.' + textTable[i]).innerHTML.trim().length;
+
+        document.querySelector(blockTable[i]).style.width = `${wid + (curBlock * 65)}px`;
+    }
+
+}
+
