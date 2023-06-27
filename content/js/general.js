275017a11de8254867a3461010b2ce6c5ca9d430
PlumpAndProud <f.machi03@gmail.com>
2023-06-27 10:17:39 +0200

    +placeholder input 5/5, +stylowanie 5/5

--- content/js/general.js
+++ content/js/general.js
@@ -1,5 +1,6 @@
 let blockTable = ['.first-block', '.second-block', '.third-block', '.fourth-block'];
 let textTable = ['first-text', 'second-text', 'third-text', 'fourth-text'];
+let inputTable = ['#input-first', '#input-second', '#input-third', '#input-fourth'];
 
 // API handler
 
@@ -11,13 +12,15 @@ $.ajax({
         for (let i = 0; i < blockTable.length; i++) {
             let $box = $(blockTable[i]);
             $box.append(`<div class="text-1 ${textTable[i]}">` + data.results[i].name.first + ' ' + data.results[i].name.last + '</div>');
+
+            document.querySelector(inputTable[i]).placeholder = data.results[i].name.first + ' ' + data.results[i].name.last;
         }
 
         setWidth();
     }
 });
 
-//brown-box width
+//brown-box auto width
 
 function setWidth() {
 
