e96d75702b917e79f6ec4ce18e799f19155c19a6
PlumpAndProud <f.machi03@gmail.com>
2023-07-06 13:10:31 +0200

    +right side api(onclick), +right side html, css, +left side grid changes px -> vw

--- content/js/weatherVue.js
+++ content/js/weatherVue.js
@@ -7,16 +7,16 @@ new Vue({
         api: null,
         data: [],
         dataForecast: [
-            {},
-            {},
-            {}
+            { date: '', icon: '', maxtemp: '', mintemp: '' },
+            { date: '', icon: '', maxtemp: '', mintemp: '' },
+            { date: '', icon: '', maxtemp: '', mintemp: '' }
         ],
     },
     methods: {
         async apiCall() {
             if (this.location != '') {
                 try {
-                    let response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=f38bf32f03504e5c822110641230507q=${this.location}&days=3&aqi=no`)
+                    let response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=f38bf32f03504e5c822110641230507&q=${this.location}&days=3&aqi=no`)
                     this.api = response.data
                 } catch (e) {
                     alert(e.message + ' ' + e.code)
@@ -32,7 +32,15 @@ new Vue({
                     `${this.api.current.humidity}`,
                     `${this.api.current.gust_kph}`
                 ]
-                // this.dataForecast
+
+                for (let i = 0; i < 3; i++) {
+                    let forecastDay = this.api.forecast.forecastday[i]
+
+                    this.dataForecast[i].date = forecastDay.date.substring(5)
+                    this.dataForecast[i].icon = forecastDay.day.condition.icon
+                    this.dataForecast[i].maxtemp = forecastDay.day.maxtemp_c
+                    this.dataForecast[i].mintemp = forecastDay.day.mintemp_c
+                }
             }
         },
 
@@ -55,6 +63,7 @@ new Vue({
         }
     },
     mounted: async function () {
+
         try {
             let response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=f38bf32f03504e5c822110641230507&q=Gliwice&days=3&aqi=no`)
             this.api = response.data
@@ -72,5 +81,14 @@ new Vue({
             `${this.api.current.humidity}`,
             `${this.api.current.gust_kph}`
         ]
+
+        for (let i = 0; i < 3; i++) {
+            let forecastDay = this.api.forecast.forecastday[i]
+
+            this.dataForecast[i].date = forecastDay.date.substring(5)
+            this.dataForecast[i].icon = forecastDay.day.condition.icon
+            this.dataForecast[i].maxtemp = forecastDay.day.maxtemp_c
+            this.dataForecast[i].mintemp = forecastDay.day.mintemp_c
+        }
     },
 })
